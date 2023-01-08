7dd088b801a2f3fd578a288fe5425d04f3228d69
Angelo Punzalan <apunzalan@berkeley.edu>
2023-01-08 05:30:36 -0800

    added how many wrong

--- languagescsv.py
+++ languagescsv.py
@@ -3,7 +3,6 @@ import numpy as np
 import random
 import io
 
-### languages = pd.read_csv(r"C:\Users\kamag\OneDrive\Desktop\languagetime\languagaguaguaguaguege-time\Languages.csv")
 url = 'https://raw.githubusercontent.com/forxer/languages-list/master/src/Languages.csv'
 
 languages = pd.read_csv(url)
@@ -16,7 +15,7 @@ def generate_random_number(start, stop):
     return random_num
  
 def display_hint(random_number_generated):
-    print(native_scripts[random_number_generated])
+    print("the native script is: " + native_scripts[random_number_generated])
 
 def correct_answer(random_number):
     correct = language_names[random_number]
@@ -24,28 +23,35 @@ def correct_answer(random_number):
 
 def guessing_game(score):
     game_state = True
+    correct_guesses = []
+    incorrect_guesses = []
     while game_state:
-        random_number = generate_random_number(0, 184)
+        random_number = generate_random_number(0, len(language_names) - 1)
         display_hint(random_number)
         language_answer = correct_answer(random_number)
         user_answer = input()
         if user_answer.strip().lower() == language_answer.strip().lower():
             print("You got it!")
+            correct_guesses.append(language_answer)
             score += 1
             game_state = False
         elif user_answer.strip().lower() == "no":
             print("the answer was " + language_answer)
+            incorrect_guesses.append(language_answer)
             game_state = False    
         elif user_answer.strip().lower() != language_answer.strip().lower():
-            print("you suck")
+            print("nice try, the language is " + str(language_answer))
+            incorrect_guesses.append(language_answer)
     play_again = "y"
     dont_play = "n"
-    print("play again? [y]es or [n]o?")
+    ### something wrong here, check scores mayhaps
+    print("game over :D do you want to play again? [y]es or [n]o?")
     choice = input().strip().lower()
     if choice == play_again:
         guessing_game(score)
     elif choice == dont_play:
-        print("thank u for playing! your score is: " + str(score))
+        print("thank u for playing! your score is: " + str(score) + " and you got " + str(len(incorrect_guesses)) + " wrong!")
+        print(incorrect_guesses)
         
 
 
