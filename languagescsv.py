6dece8a0657c3e2e301ccccf421b510ca49328b2
Angelo Punzalan <apunzalan@berkeley.edu>
2023-01-07 22:01:44 -0800

    changed so we get from github

--- languagescsv.py
+++ languagescsv.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import numpy as np
+import io
 
-languages = pd.read_csv(r"C:\Users\kamag\OneDrive\Desktop\languagetime\languagaguaguaguaguege-time\Languages.csv")
+### languages = pd.read_csv(r"C:\Users\kamag\OneDrive\Desktop\languagetime\languagaguaguaguaguege-time\Languages.csv")
+url = 'https://raw.githubusercontent.com/forxer/languages-list/master/src/Languages.csv'
+
+languages = pd.read_csv(url)
+
 language_names = languages.iloc[:, 3].array
 native_scripts = languages.iloc[:, 4].array
 print(language_names)
