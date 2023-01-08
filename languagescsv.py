85a74f27f49529aa8dc50a1d36d95e5a085a88ef
Angelo Punzalan <apunzalan@berkeley.edu>
2023-01-07 21:25:23 -0800

    used this

--- languagescsv.py
+++ languagescsv.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
 
-languages = pd.read_csv(r"C:\Users\kamag\Downloads\Languages.csv")
-language_names = languages[["Language"]]
-print(language_names)
+languages = pd.read_csv(r"C:\Users\kamag\OneDrive\Desktop\languagetime\languagaguaguaguaguege-time\Languages.csv")
+names = languages.iloc[:, 3]
+print(names)
