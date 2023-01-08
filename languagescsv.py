dd2161321fa4271b220d1597bf43daf2025655d4
Angelo Punzalan <apunzalan@berkeley.edu>
2023-01-07 21:34:35 -0800

    made arrays

--- languagescsv.py
+++ languagescsv.py
@@ -2,5 +2,7 @@ import pandas as pd
 import numpy as np
 
 languages = pd.read_csv(r"C:\Users\kamag\OneDrive\Desktop\languagetime\languagaguaguaguaguege-time\Languages.csv")
-names = languages.iloc[:, 3]
-print(names)
+language_names = languages.iloc[:, 3].array
+native_scripts = languages.iloc[:, 4].array
+print(language_names)
+print(native_scripts)
