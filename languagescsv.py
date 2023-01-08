9ceba680593af5982f3791274ab679a2a11262b4
Angelo Punzalan <apunzalan@berkeley.edu>
2023-01-07 22:34:44 -0800

    prototype

--- languagescsv.py
+++ languagescsv.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import random
 import io
 
 ### languages = pd.read_csv(r"C:\Users\kamag\OneDrive\Desktop\languagetime\languagaguaguaguaguege-time\Languages.csv")
@@ -9,5 +10,23 @@ languages = pd.read_csv(url)
 
 language_names = languages.iloc[:, 3].array
 native_scripts = languages.iloc[:, 4].array
+
 print(language_names)
 print(native_scripts)
+
+
+def answer():
+    randomized_number = random.randint(0, 185)
+    correct_answer = language_names[randomized_number]
+    displayed_hint = native_scripts[randomized_number]
+    print(displayed_hint)
+    user_answer = input()
+    while user_answer.strip().lower() != correct_answer.strip().lower():
+        print("bad try again")
+        user_answer = input()
+        if user_answer == "i hate this game":
+            answer()
+    print("asdlkfasjf")
+    
+
+answer()
